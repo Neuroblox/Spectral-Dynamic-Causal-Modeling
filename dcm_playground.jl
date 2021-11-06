c66bd27deafbce7732e2906e85013f8fe6bb8ffe
David Hofmann <david.hofmann@mytum.de>
2021-11-06 02:49:50 +0100

    tested the hemodynamics and compared with SPM12 version - the test run gave identical results. Started stiching it together with the cross spectral density computation, that is work in progress...

--- dcm_playground.jl
+++ dcm_playground.jl
@@ -1,76 +1,78 @@
 using ModelingToolkit, OrdinaryDiffEq, Plots
+using ForwardDiff: jacobian
+using LinearAlgebra
 
 # https://juliapackages.com/p/modelingtoolkit
 # https://mtk.sciml.ai/stable/systems/ODESystem/
 
 function hemodynamics(x, decay, transit)
-      """
-      Components of x are:
-      x[:,1] - neural activity: x
-      x[:,2] - vascular signal: s
-      x[:,3] - rCBF: ln(f)
-      x[:,4] - venous volume: ln(ν)
-      x[:,5] - deoxyhemoglobin (dHb): ln(q)
-      decay, transit - free parameters, set to 0 for standard parameters.
-      
-      This function implements the hymodynamics model (balloon model and neurovascular state eq.) described in: 
-      
-          Stephan KE, Weiskopf N, Drysdale PM, Robinson PA, Friston KJ (2007)
-          Comparing hemodynamic models with DCM. NeuroImage 38: 387-401.
-      
-          adapted from spm_fx_fmri.m in SPM12 by Karl Friston & Klaas Enno Stephan
-      """
-      
-      #= hemodynamic parameters
-          H(1) - signal decay                                   d(ds/dt)/ds)
-          H(2) - autoregulation                                 d(ds/dt)/df)
-          H(3) - transit time                                   (t0)
-          H(4) - exponent for Fout(v)                           (alpha)
-          H(5) - resting oxygen extraction                      (E0)
-      =#
-      H = [0.64, 0.32, 2.00, 0.32, 0.4];
-      
-      # exponentiation of hemodynamic state variables
-      x[:, 3:5] = exp(x[:, 3:5])
-      
-      # signal decay
-      κ = H[1]*exp(decay)
-      
-      # transit time
-      τ = H[3]*exp(transit)
-      
-      # Fout = f(v) - outflow
-      fv = x[:, 3].^(1/H[4])
-      
-      # e = f(f) - oxygen extraction
-      ff = (1 - (1 - H[5]).^(1.0./x[:, 3]))/H[5]
-      
-      
-      # implement differential state equation f = dx/dt (hemodynamic)
-      f[:, 2] = x[:, 1] - κ.*x[:, 2] - H[2]*(x[:, 3] - 1)   # Corresponds to eq (9)
-      f[:, 3] = x[:, 2]./x[:, 3]  # Corresponds to eq (10), note the added logarithm (see doc string)
-      f[:, 4] = (x[:, 3] - fv)./(τ.*x[:, 4])    # Corresponds to eq (8), note the added logarithm (see doc string)
-      f[:, 5] = (ff.*x[:, 3] - fv.*x[:, 5]./x[:, 4])./(τ.*x[:, 5])  # Corresponds to eq (8), note the added logarithm (see doc string)
-      
-      return f
+"""
+Components of x are:
+x[:,1] - neural activity: x
+x[:,2] - vascular signal: s
+x[:,3] - rCBF: ln(f)
+x[:,4] - venous volume: ln(ν)
+x[:,5] - deoxyhemoglobin (dHb): ln(q)
+decay, transit - free parameters, set to 0 for standard parameters.
+
+This function implements the hymodynamics model (balloon model and neurovascular state eq.) described in: 
+
+Stephan KE, Weiskopf N, Drysdale PM, Robinson PA, Friston KJ (2007)
+Comparing hemodynamic models with DCM. NeuroImage 38: 387-401.
+
+adapted from spm_fx_fmri.m in SPM12 by Karl Friston & Klaas Enno Stephan
+"""
+
+#= hemodynamic parameters
+H(1) - signal decay                                   d(ds/dt)/ds)
+H(2) - autoregulation                                 d(ds/dt)/df)
+H(3) - transit time                                   (t0)
+H(4) - exponent for Fout(v)                           (alpha)
+H(5) - resting oxygen extraction                      (E0)
+=#
+H = [0.64, 0.32, 2.00, 0.32, 0.4];
+
+# exponentiation of hemodynamic state variables
+x[:, 3:5] = exp(x[:, 3:5])
+
+# signal decay
+κ = H[1]*exp(decay)
+
+# transit time
+τ = H[3]*exp(transit)
+
+# Fout = f(v) - outflow
+fv = x[:, 3].^(1/H[4])
+
+# e = f(f) - oxygen extraction
+ff = (1 - (1 - H[5]).^(1.0./x[:, 3]))/H[5]
+
+
+# implement differential state equation f = dx/dt (hemodynamic)
+f[:, 2] = x[:, 1] - κ.*x[:, 2] - H[2]*(x[:, 3] - 1)   # Corresponds to eq (9)
+f[:, 3] = x[:, 2]./x[:, 3]  # Corresponds to eq (10), note the added logarithm (see doc string)
+f[:, 4] = (x[:, 3] - fv)./(τ.*x[:, 4])    # Corresponds to eq (8), note the added logarithm (see doc string)
+f[:, 5] = (ff.*x[:, 3] - fv.*x[:, 5]./x[:, 4])./(τ.*x[:, 5])  # Corresponds to eq (8), note the added logarithm (see doc string)
+
+return f
 end
 
 # Simulate the BOLD signal
-function g_fmri(x, ϵ)
+function boldsignal(x, lnϵ)
       """
       Simulated BOLD response to input
       FORMAT [g,dgdx] = g_fmri(x, ϵ)
       g          - BOLD response (%)
       x          - hemodynamic state vector, same as above.
       ϵ          - free parameter (note also here as above, actually ln(ϵ)), ratio of intra- to extra-vascular components
-  
+
       This function implements the BOLD signal model described in: 
-  
+
       Stephan KE, Weiskopf N, Drysdale PM, Robinson PA, Friston KJ (2007)
       Comparing hemodynamic models with DCM. NeuroImage 38: 387-401.
-   
+
       adapted from spm_gx_fmri.m in SPM12 by Karl Friston & Klaas Enno Stephan
-  
+
       NB: Biophysical constants for 1.5T scanners:
       TE  = 0.04
       V0  = 4    
@@ -78,12 +80,12 @@ function g_fmri(x, ϵ)
       nu0 = 40.3
       E0  = 0.4
       """
-  
+
       # NB: Biophysical constants for 1.5T scanners
       # Time to echo
       TE  = 0.04
       # resting venous volume (%)
-      V0  = 4    
+      V0  = 4
       # slope r0 of intravascular relaxation rate R_iv as a function of oxygen 
       # saturation S:  R_iv = r0*[(1 - S)-(1 - S0)] (Hz)
       r0  = 25
@@ -92,48 +94,98 @@ function g_fmri(x, ϵ)
       # resting oxygen extraction fraction
       E0  = 0.4
       # estimated region-specific ratios of intra- to extra-vascular signal 
-      ep  = exp(ϵ)
-  
+      ϵ  = exp(lnϵ)
+
       # -Coefficients in BOLD signal model
       k1  = 4.3*nu0*E0*TE;
-      k2  = ep*r0*E0*TE;
-      k3  = 1 - ep;
-  
+      k2  = ϵ*r0*E0*TE;
+      k3  = 1 - ϵ;
       # -Output equation of BOLD signal model
       ν   = exp(x[:,4])
       q   = exp(x[:,5])
-      g   = V0*(k1 - k1*q + k2 - k2*q./ν + k3 - k3*ν)
+      bold = V0*(k1 .- k1*q .+ k2 .- k2*q./ν .+ k3 .- k3*ν)
+
+    J[4nd+1:5nd,2nd+1:3nd] = diagm(-V0*(k3*ν .- k2*q./ν))
+    J[4nd+1:5nd,3nd+1:4nd] = diagm(-V0*(k1*q .+ k2*q./ν))
+
+    return (bold, J)
+end
   
-  
-      #= -derivative dgdx   # still need to figure out - these derivatives are strange!
-      n = size(x)[1];
-      dgdx = cell(1,m);
-      [dgdx{:}]  = deal(sparse(n,n));
-      dgdx{1,4}  = diag(-V0*(k3.*v - k2.*q./v));
-      dgdx{1,5}  = diag(-V0*(k1.*q + k2.*q./v));
-      dgdx       = spm_cat(dgdx);
-      =#
-  
-      return g
-  end
+using StaticArrays
+A  = @SMatrix [-0.5 -0.2  0.0
+               0.4 -0.5 -0.3
+               0.0  0.2 -0.5]
+f(x,A,t) = A*x
 
 using MAT
 vars = matread("spectralDCM_demodata.mat")
 Y_mat = vars["Y"]
 y_csd = vars["csd"]
-
+w = vars["M"]["Hz"]
+θμ = vars["M"]["pE"]["A"]    # see table 1 in friston2014 for values of priors 
+nd = size(θμ, 1)
+nw = length(w)
 # priors of spectral parameters
-# log(α) and log(β)
+# log(α) and log(β), region specific fluctuations: γ
 
 # define function that implements spectra given in equation (2) of "A DCM for resting state fMRI".
 
-G = w.^(-exp(β[1]))   # spectrum of hidden dynamics
+# neuronal fluctuations (Gu) (1/f or AR(1) form)
+Gu = zeros(nw, nd, nd);
+for i = 1:nd
+    G = w.^(-exp(β[1]))   # spectrum of hidden dynamics
+    Gu[:, i, i] += + exp(β)*G/sum(G);
+end
+
+
 # spectrum of visible units
 # compute Volterra kernels
-# 1. compute jacobian w.r.t. f, 2. compute also delay operator, 3. compute jacobian w.r.t. g
-J_x = jacobian(f, x0)
-bold = x -> g_fmri(hemodynamics(x, 0, 0), 0)
-J_g = jacobian(bold, x0)   # compare with analytical solution
+# 1. compute jacobian w.r.t. f ; TODO: what is it with this "delay operator" that is set to 1 in "spm_fx_fmri.m"
+# J_x = jacobian(f, x0) # well, no need to perform this for a linear system...
+J_x = Ep
+
+# 2. get jacobian of hemodynamics
+dx, J = hemodynamics(x, 0, 0)
+J_tot = [Ep, zeros(size(Ep)[1], size(J)[2]);
+         zeros(size(J)[1], size(Ep)[2]), J]
+
+[V, Λ] = eigen(J_tot)
+
+# condition unstable eigenmodes
+# if max(w) > 1
+#     s = 1j*imag(s) + real(s) - exp(real(s));
+# else
+#     s = 1j*imag(s) + min(real(s),-1/32);
+# end
+
+
+# 3. get jacobian (??) of bold signal, just compute it as is done, but how is this a jacobian, it isn't! if anything it should a gradient since the BOLD signal is scalar
+#TODO: implement numerical and compare with analytical: J_g = jacobian(bold, x0)
+g, J_g = boldsignal(x, lnϵ)
+
+dgdv  = J_g*V
+w = M.Hz(:)
+
+nw = size(w,1)            # number of frequencies
+ng = size(dgdx,1)         # number of outputs
+nk = size(v,2)            # number of modes
+S = zeros(nw,ng)
+
+for i = 1:ng
+    for k = 1:nk
+        # transfer functions (FFT of kernel)
+        Sk = 1.0/(1im*2*pi*w - Λ(k,k))
+        S[:,i] = S[:,i] .+ dgdv[i,k]*Sk
+    end
+end
+
+# predicted cross-spectral density
+G = zeros(nw,nd,nd);
+for i = 1:nw
+    G[i,:,:] = reshape(S(i,:,:),nn,nn)*reshape(Gu(i,:,:),nn,nn)*reshape(S(i,:,:),nn,nn)';
+end
+
+
 
 ##### Nonlinear System #####
 
@@ -174,13 +226,6 @@ plot(nonlinsol, vars=(1,2))
 
 
 ##### Linear System #####
-using StaticArrays, DifferentialEquations
-A  = @SMatrix [-0.5 -0.2  0.0
-                0.4 -0.5 -0.3
-                0.0  0.2 -0.5]
-u0 = rand(3)
-tspan = (0.0, 10.0)
-f(u,p,t) = A*u
 
 linprob = ODEProblem(f, u0, tspan)
 linsol = solve(linprob, saveat=0.1)
--- hemodynamic_response.jl
+++ hemodynamic_response.jl
@@ -1,23 +1,23 @@
 # Hemodynamics and bold signal all in one
 
-function hemodynamics!(J, f, x, decay, transit, ϵ)
-    """
-    Components of x are:
-    x[:,1] - neural activity: x
-    x[:,2] - vascular signal: s
-    x[:,3] - rCBF: ln(f)
-    x[:,4] - venous volume: ln(ν)
-    x[:,5] - deoxyhemoglobin (dHb): ln(q)
-    x[:,6] - BOLD signal
-    decay, transit - free parameters, set to 0 for standard parameters.
+function hemodynamics!(dx, x, na, decay, transit)
+"""
+Components of x are:
+na - neural activity: x
+x[:,1] - vascular signal: s
+x[:,2] - rCBF: ln(f)
+x[:,3] - venous volume: ln(ν)
+x[:,4] - deoxyhemoglobin (dHb): ln(q)
 
-    This function implements the hymodynamics model (balloon model and neurovascular state eq.) described in: 
+decay, transit - free parameters, set to 0 for standard parameters.
 
-        Stephan KE, Weiskopf N, Drysdale PM, Robinson PA, Friston KJ (2007)
-        Comparing hemodynamic models with DCM. NeuroImage 38: 387-401.
+This function implements the hymodynamics model (balloon model and neurovascular state eq.) described in: 
 
-        adapted from spm_fx_fmri.m in SPM12 by Karl Friston & Klaas Enno Stephan
-    """
+Stephan KE, Weiskopf N, Drysdale PM, Robinson PA, Friston KJ (2007)
+Comparing hemodynamic models with DCM. NeuroImage 38: 387-401.
+
+adapted from spm_fx_fmri.m in SPM12 by Karl Friston & Klaas Enno Stephan
+"""
 
     #= hemodynamic parameters
         H(1) - signal decay                                   d(ds/dt)/ds)
@@ -29,7 +29,7 @@ function hemodynamics!(J, f, x, decay, transit, ϵ)
     H = [0.64, 0.32, 2.00, 0.32, 0.4]
 
     # exponentiation of hemodynamic state variables
-    x[:, 3:5] = exp(x[:, 3:5])
+    x[:, 2:4] = exp.(x[:, 2:4])
 
     # signal decay
     κ = H[1]*exp(decay)
@@ -38,61 +38,36 @@ function hemodynamics!(J, f, x, decay, transit, ϵ)
     τ = H[3]*exp(transit)
 
     # Fout = f(v) - outflow
-    fv = x[:, 3].^(1/H[4])
+    fv = x[:, 2].^(H[4]^-1)
 
     # e = f(f) - oxygen extraction
-    ff = (1 - (1 - H[5]).^(1.0./x[:, 3]))/H[5]
+    ff = (1.0 .- (1.0 - H[5]).^(x[:, 2].^-1))/H[5]
 
     # implement differential state equation f = dx/dt (hemodynamic)
-    f[:, 2] = x[:, 1] - κ.*x[:, 2] - H[2]*(x[:, 3] - 1)   # Corresponds to eq (9)
-    f[:, 3] = x[:, 2]./x[:, 3]  # Corresponds to eq (10), note the added logarithm (see doc string)
-    f[:, 4] = (x[:, 3] - fv)./(τ.*x[:, 4])    # Corresponds to eq (8), note the added logarithm (see doc string)
-    f[:, 5] = (ff.*x[:, 3] - fv.*x[:, 5]./x[:, 4])./(τ.*x[:, 5])  # Corresponds to eq (8), note the added logarithm (see doc string)
+    dx[:, 1] = na .- κ.*x[:, 1] .- H[2]*(x[:, 2] .- 1)   # Corresponds to eq (9)
+    dx[:, 2] = x[:, 1]./x[:, 2]  # Corresponds to eq (10), note the added logarithm (see doc string)
+    dx[:, 3] = (x[:, 2] .- fv)./(τ.*x[:, 3])    # Corresponds to eq (8), note the added logarithm (see doc string)
+    dx[:, 4] = (ff.*x[:, 2] .- fv.*x[:, 4]./x[:, 3])./(τ.*x[:, 4])  # Corresponds to eq (8), note the added logarithm (see doc string)
 
-        # NB: Biophysical constants for 1.5T scanners
-    # Time to echo
-    TE  = 0.04
-    # resting venous volume (%)
-    V0  = 4    
-    # slope r0 of intravascular relaxation rate R_iv as a function of oxygen 
-    # saturation S:  R_iv = r0*[(1 - S)-(1 - S0)] (Hz)
-    r0  = 25
-    # frequency offset at the outer surface of magnetized vessels (Hz)
-    nu0 = 40.3
-    # resting oxygen extraction fraction
-    E0  = 0.4
-    # estimated region-specific ratios of intra- to extra-vascular signal 
-    ep  = exp(ϵ)
-
-    # -Coefficients in BOLD signal model
-    k1  = 4.3*nu0*E0*TE;
-    k2  = ep*r0*E0*TE;
-    k3  = 1 - ep;
-
-    # -Output equation of BOLD signal model
-    ν   = exp(x[:,4])
-    q   = exp(x[:,5])
-    f[:,6]   = V0*(k1 - k1*q + k2 - k2*q./ν + k3 - k3*ν)   # BOLD signal
-
-    nd = size(x)[1]
-    J[1:nd,1:nd] = Matrix(-sd*I, nd, nd)
-    J[1:nd,nd+1:2nd] = diagm(-H(2)*x[:,3])
-    J[nd+1:2nd,1:nd] = diagm( x[:,3].^-1)
-    J[nd+1:2nd,nd+1:2nd] = diagm(-x[:,2]./x[:,3])
-    J[2nd+1:3nd,nd+1:2nd] = diagm( x[:,3]./(tt.*x[:,4]))
-    J[2nd+1:3nd,2nd+1:3nd] = diagm(-x[:,4].^(1/H[4] - 1)./(tt*H[4]) - (x[:,4].^-1 .*(x[:,3] - x[:,4].^(1/H[4])))./tt)
-    J[3nd+1:4nd,nd+1:2nd] = diagm((x[:,3] + log(1 - H[5]).*(1 - H[5]).^(1.0/x[:,3]) - x[:,3].*(1 - H[5]).^(x[:,3].^-1))./(tt.*x[:,5]*H[5]))
-    J[3nd+1:4nd,2nd+1:3nd] = diagm((x[:,4].^(1/H[4] - 1)*(H[4] - 1))./(tt*H[4]))
-    J[3nd+1:4nd,3nd+1:4nd] = diagm((x[:,3]./x[:,5]).*((1 - H[5]).^(1.0/x[:,3]) - 1)./(tt*H[5]))
-    J[4nd+1:5nd,2nd+1:3nd] = diagm(-V0*(k3.*ν - k2.*q./ν))
-    J[4nd+1:5nd,3nd+1:4nd] = diagm(-V0*(k1.*q + k2.*q./ν))
+    d = size(x)[1]   # number of dimensions, equals typically number of regions
+    J = zeros(4d, 4d)
+    @show J
+    J[1:d,1:d] = Matrix(-κ*I, d, d)     # TODO: make it work when κ is a vector. Only solution if-clause? diagm doesn't allow scalars, [κ] would work in that case
+    J[1:d,d+1:2d] = diagm(-H[2]*x[:,2])
+    J[d+1:2d,1:d] = diagm( x[:,2].^-1)
+    J[d+1:2d,d+1:2d] = diagm(-x[:,1]./x[:,2])
+    J[2d+1:3d,d+1:2d] = diagm(x[:,2]./(τ.*x[:,3]))
+    J[2d+1:3d,2d+1:3d] = diagm(-x[:,3].^(H[4]^-1 - 1)./(τ*H[4]) - (x[:,3].^-1 .*(x[:,2] - x[:,3].^(H[4]^-1)))./τ)
+    J[3d+1:4d,d+1:2d] = diagm((x[:,2] .+ log(1 - H[5])*(1 - H[5]).^(x[:,2].^-1) .- x[:,2].*(1 - H[5]).^(x[:,2].^-1))./(τ.*x[:,4]*H[5]))
+    J[3d+1:4d,2d+1:3d] = diagm((x[:,3].^(H[4]^-1 - 1)*(H[4] - 1))./(τ*H[4]))
+    J[3d+1:4d,3d+1:4d] = diagm((x[:,2]./x[:,4]).*((1 - H[5]).^(x[:,2].^-1) .- 1)./(τ*H[5]))
     
-    return f
+    return (dx, J)
 end
 
 
 # Simulate the BOLD signal
-function g_fmri(x, ϵ)
+function boldsignal(x, lnϵ)
     """
     Simulated BOLD response to input
     FORMAT [g,dgdx] = g_fmri(x, ϵ)
@@ -128,18 +103,20 @@ function g_fmri(x, ϵ)
     # resting oxygen extraction fraction
     E0  = 0.4
     # estimated region-specific ratios of intra- to extra-vascular signal 
-    ep  = exp(ϵ)
+    ϵ  = exp(lnϵ)
 
     # -Coefficients in BOLD signal model
     k1  = 4.3*nu0*E0*TE;
-    k2  = ep*r0*E0*TE;
-    k3  = 1 - ep;
+    k2  = ϵ*r0*E0*TE;
+    k3  = 1 - ϵ;
 
     # -Output equation of BOLD signal model
     ν   = exp(x[:,4])
     q   = exp(x[:,5])
-    g   = V0*(k1 - k1*q + k2 - k2*q./ν + k3 - k3*ν)
+    bold = V0*(k1 .- k1*q .+ k2 .- k2*q./ν .+ k3 .- k3*ν)
 
+    J[4nd+1:5nd,2nd+1:3nd] = diagm(-V0*(k3*ν .- k2*q./ν))
+    J[4nd+1:5nd,3nd+1:4nd] = diagm(-V0*(k1*q .+ k2*q./ν))
 
-    return g
+    return (bold, J)
 end
