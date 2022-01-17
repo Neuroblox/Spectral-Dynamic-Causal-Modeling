cf0c685ef2c2ffda33b8a1275cc5b8f03d538f6c
David Hofmann <david.hofmann@mytum.de>
2022-01-17 18:37:14 +0100

    finished spectral DCM main loop, testing ahead

--- spectralDCM.jl
+++ spectralDCM.jl
@@ -46,7 +46,7 @@ function hemodynamics!(dx, x, na, decay, transit)
     τ = H[3]*exp.(transit)
 
     # Fout = f(v) - outflow
-    fv = x[:, 2].^(H[4]^-1)
+    fv = x[:, 3].^(H[4]^-1)
 
     # e = f(f) - oxygen extraction
     ff = (1.0 .- (1.0 - H[5]).^(x[:, 2].^-1))/H[5]
@@ -322,6 +322,17 @@ function diff(U, dx, f, param)
     return J, y0
 end
 
+function matlab_norm(A, p)
+    if p == 1
+        return maximum(vec(sum(abs.(A),dims=1)))
+    elseif p == Inf
+        return maximum(vec(sum(abs.(A),dims=2)))
+    elseif p == 2
+        print("Not implemented yet!\n")
+        return NaN
+    end
+end
+
 function csd_Q(csd)
     s = size(csd)
     Qn = length(csd)
@@ -334,8 +345,7 @@ function csd_Q(csd)
             end
         end
     end
-    norm_matlab = maximum(vec(sum(abs.(Q),dims=2)))
-    Q = inv(Q .+ norm_matlab/32*Matrix(I, size(Q)))   # TODO: MATLAB's and Julia's norm function are different! Reconciliate?
+    Q = inv(Q .+ matlab_norm(Q, 1)/32*Matrix(I, size(Q)))   # TODO: MATLAB's and Julia's norm function are different! Reconciliate?
     return Q
 end
 
@@ -346,21 +356,10 @@ vars = matread("spectralDCM_demodata_notsparse.mat")
 # Y_mat = vars["Y"]
 y_csd = vars["csd"]
 w = vec(vars["M_nosparse"]["Hz"])
-θμ = vars["M_nosparse"]["pE"]["A"]    # see table 1 in friston2014 for values of priors 
+A = vars["M_nosparse"]["pE"]["A"]    # see table 1 in friston2014 for values of priors 
 θΣ = vars["M_nosparse"]["pC"]
-λE = vars["M_nosparse"]["hE"]
+λμ = vars["M_nosparse"]["hE"]
 Πλ_p = vars["M_nosparse"]["ihC"]
-np = length(θμ)            # number of parameters
-ny = length(y_csd)         # total number of response variables
-ns = size(y_csd, 1)        # number of samples
-nr = ny÷ns                 # number of response components
-nq = 1
-Q = zeros(ny,ny,nr)
-for i = 1:nr
-    Q[((i-1)*ns+1):(i*ns), ((i-1)*ns+1):(i*ns), i] = Matrix(1.0I, ns, ns)
-end
-nh = size(Q,3)             # number of precision components (this is the same as above, but may differ)
-λ = 8 * ones(nh)
 
 idx = findall(x -> x != 0, θΣ)
 V = zeros(size(θΣ, 1), length(idx))
@@ -372,7 +371,7 @@ end
 θΣ = V'*θΣ*V
 Πθ_p = inv(θΣ)
 
-dim = size(θμ, 1)
+dim = size(A, 1)
 C = zeros(Float64, dim)    # NB: whatever C is defined to be here, it will be replaced in csd_approx. A little strange thing of SPM12
 p = 8
 α = [0.0, 0.0]
@@ -382,78 +381,174 @@ lnϵ = 0.0                        # BOLD signal parameter
 lndecay = 0.0                    # hemodynamic parameter
 lntransit = zeros(Float64, dim)  # hemodynamic parameters
 x = zeros(Float64, 3, 5)
-param = [reshape(θμ, dim^2); C; lntransit; lndecay; lnϵ; α[1]; β[1]; α[2]; β[2]; γ;]
+param = [p; reshape(A, dim^2); C; lntransit; lndecay; lnϵ; α[1]; β[1]; α[2]; β[2]; γ;]
 # Strange α and β sorting? yes. This is to be consistent with the SPM12 code while keeping nomenclature consistent with the spectral DCM paper
-
-dx = exp(-8)
-f_prep = param -> csd_fmri_mtf(x, w, p, param)
+priors = [Πθ_p, Πλ_p]
 
 
+function VariationalBayesStep(x, y, w, V, param, priors)
+    # extract priors
+    Πθ_p = priors[1]
+    Πλ_p = priors[2]
 
-dfdp, f = diff(V, dx, f_prep, param);
-# TODO: SPM12 has a stability check here, we omit it for now.
-
-e = reshape(y_csd - f, ny)   # error value
-J = - transpose(reshape(dfdp, 19, ny))      # Jacobian, unclear why we have a minus sign. Helmut: comes from deriving a Gaussian. 
-
-
-## M-step: Fisher scoring scheme to find h = max{F(p,h)} // comment from MATLAB code
-iΣ = zeros(ny, ny)  # local iΣ
-ϵ_λ = zeros(np)
-for m = 1:8   # 8 seems arbitrary. Numbers of iterations taken from SPM12 code.
-    tmp = zeros(ny, ny)
-    for i = 1:nh
-        tmp = tmp .+ Q[:,:,i]*exp(λ[i])
+    # prep stuff
+    p = Int(param[1])
+    μθ = param[2:end]          # note: μθ is posterior and θμ is prior
+    np = size(V, 2)            # number of parameters
+    ny = length(y)             # total number of response variables
+    ns = size(y, 1)            # number of samples
+    nr = ny÷ns                 # number of response components
+    dim = sqrt(nr)             # dimension of the problem, number of regions
+    nq = 1
+    ϵ_θ = zeros(np)  # M.P - θμ # still need to figure out what M.P is for. It doesn't seem to be used further down the road in nlsi_GM, only at the very beginning when p is defined first. Then replace μθ with θμ above.
+    Q = zeros(ny,ny,nr)
+    for i = 1:nr
+        Q[((i-1)*ns+1):(i*ns), ((i-1)*ns+1):(i*ns), i] = Matrix(1.0I, ns, ns)
     end
-    iΣ = tmp
+    nh = size(Q,3)             # number of precision components (this is the same as above, but may differ)
+    λ = 8 * ones(nh)
 
-    Σ = inv(iΣ)        # Julia requires conversion to dense matrix before inversion so just use dense from the get-go
-    Pp = zeros(ComplexF64, size(Πθ_p))
-    mul!(Pp, J', iΣ * J)    # in MATLAB code 'real()' is applied to the resulting matrix product, why?
-    Pp = real(Pp)
-    Σθ = inv(Pp + Πθ_p)
+    dx = exp(-8)
+    revert = false
+    f_prep = param -> csd_fmri_mtf(x, w, p, param)
 
-    P = similar(Q)
-    PΣ = similar(Q)
-    JPJ = zeros(size(Pp,1), size(Pp,2), size(Q,3))
-    for i = 1:nh
-        P[:,:,i] = Q[:,:,i]*exp(λ[i])
-        PΣ[:,:,i] = P[:,:,i] * Σ
-        JPJ[:,:,i] = real(J'*P[:,:,i]*J)      # in MATLAB code 'real()' is applied (see also some lines above), what's the rational?
-    end
+    # state variable
+    state["F"] = -Inf
+    v = -4   # log ascent rate
+    
+    for k = 1:10
 
-    dFdh = zeros(nh)
-    dFdhh = zeros(nh, nh)
-    for i = 1:nh
-        dFdh[i] = (tr(PΣ[:,:,i])*nq - real(e'*P[:,:,i]*e) - tr(Σθ * JPJ[:,:,i]))/2
-        for j = i:nh
-            dFdhh[i, j] = - tr(PΣ[:,:,i] * PΣ[:,:,j])*nq/2
-            dFdhh[j, i] = dFdhh[i, j]
+        dfdp, f = diff(V, dx, f_prep, μθ);
+        norm_dfdp = matlab_norm(dfdp, Inf);
+        revert = isnan(norm_dfdp) || norm_dfdp > exp(32);
+
+
+        if revert && k > 1
+            for i = 1:4
+                # reset expansion point and increase regularization
+                v = min(v - 2,-4);
+                v = exp(v - logdet(dFdpp)/dim)
+
+                # E-Step: update
+                if v > exp(16)
+                    ϵ_θ = state["ϵ_θ"] - inv(dFdpp)*dFdp    # -inv(dfdx)*f
+                else
+                    ϵ_θ = state["ϵ_θ"] + (expv(dFdpp*v) - I)*inv(dFdpp)*dFdp   # (expm(dfdx*t) - I)*inv(dfdx)*f
+                end
+
+                μθ = θμ + V*ϵ_θ
+
+                dfdp, f = diff(V, dx, f_prep, μθ);
+
+                # check for stability
+                norm_dfdp = matlab_norm(dfdp, Inf);
+                revert = isnan(norm_dfdp) || norm_dfdp > exp(32);
+        
+                # break
+                if ~revert
+                    break
+                end
+            end
         end
-    end
 
-    ϵ_λ = λ - λE
-    dFdh = dFdh - Πλ_p*ϵ_λ
-    dFdhh = dFdhh - Πλ_p
-    Σλ = inv(-dFdhh)
 
-    dλ = -inv(dFdhh) * dFdh    # Gauss-Newton step, transform into Levenberg-Marquardt, see MATLAB code
-    dλ = [min(max(x,-1.0),1.0) for x in dλ]      # probably precaution for numerical instabilities?
-    λ = λ + dλ
+        ϵ = reshape(y - f, ny)                   # error value
+        J = - transpose(reshape(dfdp, np, ny))   # Jacobian, unclear why we have a minus sign. Helmut: comes from deriving a Gaussian. 
 
-    dF = dot(dFdh,dλ)
-    print(dF,"\n")
-    # NB: it is unclear as to whether this is being reached. In this first tests iterations seem to be 
-    # trapped in a periodic orbit jumping around between 1250 and 940. At that point the results become
-    # somewhat arbitrary. The iterations stop at 8, whatever the last value of iΣ etc. is will be carried on.
-    if dF < 1e-2
-        break
-    end
+
+        ## M-step: Fisher scoring scheme to find h = max{F(p,h)} // comment from MATLAB code
+        Σθ = similar(Πθ_p)
+        Σλ = similar(Πθ_p)
+        for m = 1:8   # 8 seems arbitrary. Numbers of iterations taken from SPM12 code.
+            iΣ = zeros(ny, ny)
+            for i = 1:nh
+                iΣ = iΣ .+ Q[:,:,i]*exp(λ[i])
+            end
+
+            Σ = inv(iΣ)             # Julia requires conversion to dense matrix before inversion so just use dense from the get-go
+            Pp = zeros(ComplexF64, size(Πθ_p))
+            mul!(Pp, J', iΣ * J)    # in MATLAB code 'real()' is applied to the resulting matrix product, why?
+            Pp = real(Pp)
+            Σθ = inv(Pp + Πθ_p)
+
+            P = similar(Q)
+            PΣ = similar(Q)
+            JPJ = zeros(size(Pp,1), size(Pp,2), size(Q,3))
+            for i = 1:nh
+                P[:,:,i] = Q[:,:,i]*exp(λ[i])
+                PΣ[:,:,i] = P[:,:,i] * Σ
+                JPJ[:,:,i] = real(J'*P[:,:,i]*J)      # in MATLAB code 'real()' is applied (see also some lines above), what's the rational?
+            end
+
+            dFdh = zeros(nh)
+            dFdhh = zeros(nh, nh)
+            for i = 1:nh
+                dFdh[i] = (tr(PΣ[:,:,i])*nq - real(e'*P[:,:,i]*e) - tr(Σθ * JPJ[:,:,i]))/2
+                for j = i:nh
+                    dFdhh[i, j] = - tr(PΣ[:,:,i] * PΣ[:,:,j])*nq/2
+                    dFdhh[j, i] = dFdhh[i, j]
+                end
+            end
+
+            ϵ_λ = λ - λE
+            dFdh = dFdh - Πλ_p*ϵ_λ
+            dFdhh = dFdhh - Πλ_p
+            Σλ = inv(-dFdhh)
+
+            dλ = -inv(dFdhh) * dFdh    # Gauss-Newton step, transform into Levenberg-Marquardt, see MATLAB code
+            dλ = [min(max(x,-1.0),1.0) for x in dλ]      # probably precaution for numerical instabilities?
+            λ = λ + dλ
+
+            dF = dot(dFdh,dλ)
+            print(dF,"\n")
+            # NB: it is unclear as to whether this is being reached. In this first tests iterations seem to be 
+            # trapped in a periodic orbit jumping around between 1250 and 940. At that point the results become
+            # somewhat arbitrary. The iterations stop at 8, whatever the last value of iΣ etc. is will be carried on.
+            if dF < 1e-2
+                break
+            end
+        end
+
+        ## E-Step with Levenberg-Marquardt regularization    // comment from MATLAB code
+        L = zeros(3)
+        L[1] = (logdet(iΣ)*nq  - real(ϵ'*iΣ*ϵ) - ny*log(2pi))/2   # TODO: figure out why dot gives a different result here (dot(e',iΣ,e))
+        L[2] = (logdet(Πθ_p * Σθ) - dot(ϵ_θ', Πθ_p, ϵ_θ))/2
+        L[3] = (logdet(Πλ_p * Σλ) - dot(ϵ_λ', Πλ_p, ϵ_λ))/2;
+        F = sum(L);
+
+        if F > state["F"] || k < 3
+            # accept current state
+            state["F"] = F
+            state["ϵ_θ"] = ϵ_θ
+            state["λ"] = λ
+            state["Σθ"] = Σθ
+            # Conditional update of gradients and curvature
+            dFdp  = -real(J'* iΣ * e) - iΣ_θ * p   # check sign!!
+            dFdpp = -dot(J', iΣ, J) - iΣ_θ
+            # decrease regularization
+            v     = min(v + 1/2,4);
+        else
+            # reset expansion point
+            ϵ_θ = state["ϵ_θ"]
+            λ = state["λ"]
+            Σθ = state["Σθ"]
+            
+            # and increase regularization
+            v = min(v - 2,-4);
+        end
+
+        # E-Step: update
+        v = exp(v - logdet(dFdpp)/dim)
+        if v > exp(16)
+            dθ = - inv(dFdpp)*dFdp    # -inv(dfdx)*f
+        else
+            dθ = (expv(dFdpp*v) - I)*inv(dFdpp)*dFdp   # (expm(dfdx*t) - I)*inv(dfdx)*f
+        end
+
+        ϵ_θ += dθ
+        μθ = θμ + ϵ_θ
+
+    return F
 end
 
-## E-Step with Levenberg-Marquardt regularization // comment from MATLAB code
-L = zeros(3)
-L[1] = (logdet(iΣ)*nq  - real(e'*iΣ*e) - ny*log(2pi))/2   # figure out why dot gives a different result here (dot(e',iΣ,e))
-L[2] = (logdet(Πθ_p * Σθ) - dot(ϵ_θ', Πθ_p, ϵ_θ))/2
-L[3] = (logdet(Πλ_p * Σλ) - dot(ϵ_λ', Πλ_p, ϵ_λ))/2;
-F = sum(L);
+VariationalBayesStep(x, y_csd, w, V, param, priors)
