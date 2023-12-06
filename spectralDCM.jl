53aac71645768ec3e8623702210906642b8fcfc4
david-hofmann <david.hofmann@mytum.de>
2023-12-06 12:11:45 +0100

    some cleanup and fixed AD spectral DCM

--- spectralDCM.jl
+++ spectralDCM.jl
@@ -77,11 +77,9 @@ priors = Dict(:μ => OrderedDict{Any, Any}(
                         )
              );
 
-foo_old = Ref{Any}()
 ### Compute the DCM ###
-@time results = variationalbayes(x, y_csd, freqs, V, p, priors, 1);
+@time results = variationalbayes(x, y_csd, freqs, V, p, priors, 126);
 
-blabla = car
 # @benchmark bar1 = LinearAlgebra.eigen(J_tot)
 # function test(J)
 #     F = DifferentiableEigen.eigen(J_tot)
@@ -125,65 +123,65 @@ blabla = car
 # @benchmark test2()
 # @benchmark test3()
 
-function f(y,x) # in-place
-  global fcalls += 1
-  for i in 2:length(x)-1
-    y[i] = x[i-1] - 2x[i] + x[i+1]
-  end
-  y[1] = -2x[1] + x[2]
-  y[end] = x[end-1] - 2x[end]
-  nothing
-end
+# function f(y,x) # in-place
+#   global fcalls += 1
+#   for i in 2:length(x)-1
+#     y[i] = x[i-1] - 2x[i] + x[i+1]
+#   end
+#   y[1] = -2x[1] + x[2]
+#   y[end] = x[end-1] - 2x[end]
+#   nothing
+# end
 
 
 
 
 
-using SparseDiffTools
-using ForwardDiff
+# using SparseDiffTools
+# using ForwardDiff
 
-fcalls = 0
-function g(x) # out-of-place
-    global fcalls += 1
-    y = zero(x)
-    for i in 2:length(x)-1
-      y[i] = x[i-1] - 2x[i] + x[i+1]
-    end
-    y[1] = -2x[1] + x[2]
-    y[end] = x[end-1] - 2x[end]
-    y
-end
-
-
-using Plots
-
-nvarset = [10, 50, 100, 250, 500]
-ncolset = [10, 50, 100, 250, 500]
-nvarset = vcat(2,10:10:50)
-ncolset = vcat(2, 10:10:50)
-
-speed_Jfree = zeros(length(nvarset)*length(ncolset))
-speed_Jbased = zeros(length(nvarset)*length(ncolset))
-for (i, (nvars, ncols)) in enumerate(Iterators.product(nvarset, ncolset))
-    x = rand(nvars)
-    J = JacVec(g, x)
-    V = rand(nvars, ncols)
-    tmp = @benchmark stack(J*c for c in eachcol(V))
-    speed_Jfree[i] = mean(tmp.times)*1e-6
-    tmp = @benchmark ForwardDiff.jacobian(g, x)*V
-    speed_Jbased[i] = mean(tmp.times)*1e-6
-end
-p1 = contourf(nvarset, ncolset, speed_Jbased, title="Matrix-based", color=:turbo, clim=(0.0,0.04));
-p2 = contourf(nvarset, ncolset, speed_Jfree, xlabel="number of variables", title="Matrix-free", color=:turbo, clim=(0.0,0.04));
-plot(p1, p2, layout=(2, 1), ylabel="number of columns")
+# fcalls = 0
+# function g(x) # out-of-place
+#     global fcalls += 1
+#     y = zero(x)
+#     for i in 2:length(x)-1
+#       y[i] = x[i-1] - 2x[i] + x[i+1]
+#     end
+#     y[1] = -2x[1] + x[2]
+#     y[end] = x[end-1] - 2x[end]
+#     y
+# end
 
 
-res = similar(v)
-v = foo[][3][:, 1]
-J = JacVec(foo[][1], foo[][2])
-mul!(res, J, v)
-J* foo[][3]
-dfdp = zeros(Complex, 288, 21)
-for i = 1:size(foo[][3], 2)
-    dfdp[:, i] = J*foo[][3][:, i]
-end
+# using Plots
+
+# nvarset = [10, 50, 100, 250, 500]
+# ncolset = [10, 50, 100, 250, 500]
+# nvarset = vcat(2,10:10:50)
+# ncolset = vcat(2, 10:10:50)
+
+# speed_Jfree = zeros(length(nvarset)*length(ncolset))
+# speed_Jbased = zeros(length(nvarset)*length(ncolset))
+# for (i, (nvars, ncols)) in enumerate(Iterators.product(nvarset, ncolset))
+#     x = rand(nvars)
+#     J = JacVec(g, x)
+#     V = rand(nvars, ncols)
+#     tmp = @benchmark stack(J*c for c in eachcol(V))
+#     speed_Jfree[i] = mean(tmp.times)*1e-6
+#     tmp = @benchmark ForwardDiff.jacobian(g, x)*V
+#     speed_Jbased[i] = mean(tmp.times)*1e-6
+# end
+# p1 = contourf(nvarset, ncolset, speed_Jbased, title="Matrix-based", color=:turbo, clim=(0.0,0.04));
+# p2 = contourf(nvarset, ncolset, speed_Jfree, xlabel="number of variables", title="Matrix-free", color=:turbo, clim=(0.0,0.04));
+# plot(p1, p2, layout=(2, 1), ylabel="number of columns")
+
+
+# res = similar(v)
+# v = foo[][3][:, 1]
+# J = JacVec(foo[][1], foo[][2])
+# mul!(res, J, v)
+# J* foo[][3]
+# dfdp = zeros(Complex, 288, 21)
+# for i = 1:size(foo[][3], 2)
+#     dfdp[:, i] = J*foo[][3][:, i]
+# end
